95304b1be88a7409800e838adc0d03b8ca9478f1
narayanagung <agungnarayanagung@gmail.com>
2024-01-14 17:59:36 +0800

    add 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { page } from "$app/stores";
+</script>
+
+<div class="error">
+	<p>{$page.status}: {$page.error?.message}</p>
+</div>
+
+<style>
+	.error {
+		display: grid;
+		height: 100%;
+		place-content: center;
+	}
+</style>
