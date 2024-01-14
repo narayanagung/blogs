5ff6e78033f9d84c431658f0cea5c5172cc94071
narayanagung <agungnarayanagung@gmail.com>
2024-01-14 17:45:21 +0800

    page transition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,21 @@
 <script lang="ts">
 	import Header from "./header.svelte";
 	import Footer from "./footer.svelte";
+	import Transition from "./transition.svelte";
 
 	import "../app.css";
+
+	export let data;
 </script>
 
 <Header />
+
 <main>
-	<slot />
+	<Transition url={data.url}>
+		<slot />
+	</Transition>
 </main>
+
 <Footer />
 
 <style>
